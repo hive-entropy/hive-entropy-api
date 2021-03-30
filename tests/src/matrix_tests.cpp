b3dcb3f730ccd14b592052f3db67c820246d9e3d
SevenMoor <au.ottaviano@yahoo.com>
2021-03-31 00:52:54 +0200

    Update: Matrix unit tests for accessors and constructors

--- tests/src/matrix_tests.cpp
+++ tests/src/matrix_tests.cpp
@@ -23,19 +23,36 @@ SCENARIO("We should be able to create a Matrix and access its elements","[matrix
             }
 
             THEN("Its rows should be accessible and be as defined as in the array"){
-                //...
+                int* row = a.getRow(1);
+                REQUIRE(row[0]==4);
+                REQUIRE(row[1]==5);
+                REQUIRE(row[2]==6);
             }
 
             THEN("Its columns should be accessible and be as defined as in the array"){
-                //...
+                int* col = a.getColumn(1);
+                REQUIRE(col[0]==2);
+                REQUIRE(col[1]==5);
+                REQUIRE(col[2]==8);
+            }
+
+            THEN("We should be able to access a submatrix"){
+                Matrix<int> sub = a.getSubmatrix(1,1,2,2);
+
+                REQUIRE(sub.get(0,0)==5);
+                REQUIRE(sub.get(0,1)==6);
+                REQUIRE(sub.get(1,0)==8);
+                REQUIRE(sub.get(1,1)==9);
             }
         }
 
         WHEN("We change an element's value"){
             a.put(1,1,42);
+
             THEN("We should be able to access this element with its new value"){
                 REQUIRE(a.get(1,1)==42);
             }
+
             THEN("We should be able to access the element's line and column with its new value"){
                 int* col = a.getColumn(1);
                 int* row = a.getRow(1);
@@ -47,18 +64,131 @@ SCENARIO("We should be able to create a Matrix and access its elements","[matrix
                 REQUIRE(row[1]==42);
                 REQUIRE(row[2]==6);
             }
+
+            THEN("We should be able to access a submatrix with the element's new value"){
+                Matrix<int> sub = a.getSubmatrix(1,1,2,2);
+
+                REQUIRE(sub.get(0,0)==42);
+                REQUIRE(sub.get(0,1)==6);
+                REQUIRE(sub.get(1,0)==8);
+                REQUIRE(sub.get(1,1)==9);
+            }
         }
 
         WHEN("We put a whole column"){
-            //...
+            int tab[] = {1,2,3};
+            a.putColumn(1,tab);
+
+            THEN("We should be able to access the new elements individually"){
+                REQUIRE(a.get(0,1)==1);
+                REQUIRE(a.get(1,1)==2);
+                REQUIRE(a.get(2,1)==3);
+            }
+
+            THEN("We should be able to access the new elements in each rows"){
+                int* row1 = a.getRow(0);
+                int* row2 = a.getRow(1);
+                int* row3 = a.getRow(2);
+
+                REQUIRE(row1[1]==1);
+                REQUIRE(row2[1]==2);
+                REQUIRE(row3[1]==3);
+            }
+
+            THEN("We should be able to access the new elements in the column"){
+                int* col = a.getColumn(1);
+
+                REQUIRE(col[0]==1);
+                REQUIRE(col[1]==2);
+                REQUIRE(col[2]==3);
+            }
+
+            THEN("We should be able to access the new elements in a submatrix"){
+                Matrix<int> sub = a.getSubmatrix(0,1,2,2);
+
+                REQUIRE(sub.get(0,0)==1);
+                REQUIRE(sub.get(0,1)==2);
+                REQUIRE(sub.get(0,2)==3);
+            }
         }
 
         WHEN("We put a whole row"){
-            //...
+            int tab[] = {1,2,3};
+            a.putRow(1,tab);
+
+            THEN("We should be able to access the new elements individually"){
+                REQUIRE(a.get(1,0)==1);
+                REQUIRE(a.get(1,0)==2);
+                REQUIRE(a.get(1,0)==3);
+            }
+
+            THEN("We should be able to access the new elements in the row"){
+                int* row = a.getRow(1);
+
+                REQUIRE(row[0]==1);
+                REQUIRE(row[1]==2);
+                REQUIRE(row[2]==3);
+            }
+
+            THEN("We should be able to access the new elements in each column"){
+                int* col1 = a.getColumn(0);
+                int* col2 = a.getColumn(1);
+                int* col3 = a.getColumn(2);
+
+                REQUIRE(col1[1]==1);
+                REQUIRE(col2[1]==2);
+                REQUIRE(col3[1]==3);
+            }
+
+            THEN("We should be able to access the new elements in a submatrix"){
+                Matrix<int> sub = a.getSubmatrix(0,1,2,2);
+
+                REQUIRE(sub.get(0,0)==1);
+                REQUIRE(sub.get(0,1)==2);
+                REQUIRE(sub.get(0,2)==3);
+            }
         }
 
         WHEN("We put a submatrix inside the matrix"){
-            //...
+            int tab[] = {0,1,2,3};
+            Matrix<int> m(2,2,tab);
+            a.putSubmatrix(1,1,m);
+
+            THEN("We should be able to access the elements directly"){
+                REQUIRE(a.get(1,1)==0);
+                REQUIRE(a.get(1,2)==1);
+                REQUIRE(a.get(2,1)==2);
+                REQUIRE(a.get(2,2)==3);
+            }
+
+            THEN("We should be able to restituate the submatrix"){
+                Matrix<int> res = a.getSubmatrix(1,1,2,2);
+
+                REQUIRE(res.get(0,0)==m.get(0,0));
+                REQUIRE(res.get(0,1)==m.get(0,1));
+                REQUIRE(res.get(1,0)==m.get(1,0));
+                REQUIRE(res.get(1,1)==m.get(1,1));
+            }
+
+            THEN("We should be able to access the new elements inside a row"){
+                int* row1 = a.getRow(1);
+                int* row2 = a.getRow(2);
+
+                REQUIRE(row1[1]==0);
+                REQUIRE(row1[2]==1);
+                REQUIRE(row2[1]==2);
+                REQUIRE(row2[2]==3);
+            }
+
+            THEN("We should be able to access the new elements inside a column"){
+                int* col1 = a.getColumn(1);
+                int* col2 = a.getColumn(2);
+
+                REQUIRE(col1[1]==0);
+                REQUIRE(col1[2]==2);
+                REQUIRE(col2[1]==1);
+                REQUIRE(col2[2]==3);
+            }
         }
 
         WHEN("We put elements,columns,rows,submatrices outside of its bounds or of the wrong size"){
@@ -68,11 +198,72 @@ SCENARIO("We should be able to create a Matrix and access its elements","[matrix
         }
     }
 
-    GIVEN("A matrix of int defined using an archetype"){
-        //...
+    GIVEN("Matrices of int defined using  archetypes"){
+        Matrix<int> a(3,3,MatrixArchetype::ZEROS);
+        Matrix<int> b(3,3,MatrixArchetype::ONES);
+        Matrix<int> c(3,3,MatrixArchetype::ID);
+
+        THEN("Their elements should correspond to the archetype pattern"){
+            REQUIRE(a.get(0,0)==0);
+            REQUIRE(a.get(0,1)==0);
+            REQUIRE(a.get(0,2)==0);
+            REQUIRE(a.get(1,0)==0);
+            REQUIRE(a.get(1,1)==0);
+            REQUIRE(a.get(1,2)==0);
+            REQUIRE(a.get(2,0)==0);
+            REQUIRE(a.get(2,1)==0);
+            REQUIRE(a.get(2,2)==0);
+
+            REQUIRE(b.get(0,0)==1);
+            REQUIRE(b.get(0,1)==1);
+            REQUIRE(b.get(0,2)==1);
+            REQUIRE(b.get(1,0)==1);
+            REQUIRE(b.get(1,1)==1);
+            REQUIRE(b.get(1,2)==1);
+            REQUIRE(b.get(2,0)==1);
+            REQUIRE(b.get(2,1)==1);
+            REQUIRE(b.get(2,2)==1);
+
+            REQUIRE(c.get(0,0)==1);
+            REQUIRE(c.get(0,1)==0);
+            REQUIRE(c.get(0,2)==0);
+            REQUIRE(c.get(1,0)==0);
+            REQUIRE(c.get(1,1)==1);
+            REQUIRE(c.get(1,2)==0);
+            REQUIRE(c.get(2,0)==0);
+            REQUIRE(c.get(2,1)==0);
+            REQUIRE(c.get(2,2)==1);
+        }
     }
 
     GIVEN("A matrix of int defined using another matrix"){
-        //...
+        int tab[] = {1,2,3,4,5,6,7,8,9};
+        Matrix<int> a(3,3,tab);
+        Matrix<int> b = a;
+
+        THEN("The elements of both matrices should be equal"){
+            REQUIRE(a.get(0,0)==b.get(0,0));
+            REQUIRE(a.get(0,1)==b.get(0,1));
+            REQUIRE(a.get(0,2)==b.get(0,2));
+            REQUIRE(a.get(1,0)==b.get(1,0));
+            REQUIRE(a.get(1,1)==b.get(1,1));
+            REQUIRE(a.get(1,2)==b.get(1,2));
+            REQUIRE(a.get(2,0)==b.get(2,0));
+            REQUIRE(a.get(2,1)==b.get(2,1));
+            REQUIRE(a.get(2,2)==b.get(2,2));
+        }
+
+        THEN("The dimensions of the matrix should be equal"){
+            REQUIRE(a.getColumns()==b.getColumns());
+            REQUIRE(a.getRows()==b.getRows());
+        }
     }
+}
+
+SCENARIO("We should be able to do mathematical operations on matrices","[matrix-math]"){
+    //...
+}
+
+SCENARIO("We should be able to compare matrices","[matrix-logic]"){
+    //...
 }
