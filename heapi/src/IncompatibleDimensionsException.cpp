24a75e341ff68fb718064871b8cf6663485d7609
SevenMoor <au.ottaviano@yahoo.com>
2021-04-03 15:59:34 +0200

    Add: Exceptions for Matrix object

---
+++ heapi/src/IncompatibleDimensionsException.cpp
@@ -0,0 +1,16 @@
+#include "../include/IncompatibleDimensionsException.h"
+
+std::ostream& operator<<(std::ostream& os, IncompatibleDimensionsException& ex){
+    switch(ex.op){
+        case Operation::ADDITION:
+            os << "Both operators should have the same dimension to perform an addition: ("<<ex.x1<<","<<ex.y1<<")!=("<<ex.x2<<","<<ex.y2<<")";
+        break; 
+        case Operation::SUBTRACTION:
+            os << "Both operators should have the same dimension to perform a subtraction: ("<<ex.x1<<","<<ex.y1<<")!=("<<ex.x2<<","<<ex.y2<<")";
+        break;
+        case Operation::MULTIPLICATION:
+            os << "The row count of the first operator is not equal to the columns of the second operator which is required to perform a multiplication: "<<ex.x1<<"!="<<ex.y2;
+        break;
+    }
+    return os;
+}
---
+++ heapi/src/OutOfBoundException.cpp
@@ -0,0 +1,6 @@
+#include "../include/OutOfBoundException.h"
+
+std::ostream& operator<<(std::ostream& os, OutOfBoundException const& ex){
+    os << "A parameter was out of the bounds of this Matrix: " << ex.var << "=" << ex.value << " but should be between " << ex.bounds.at(0) << " and " << ex.bounds.at(1);
+    return os;
+}
