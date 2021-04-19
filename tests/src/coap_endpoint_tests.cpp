9b5d6b516a8ad54eadc78dcdd0e294b4239ad786
SevenMoor <au.ottaviano@yahoo.com>
2021-04-19 23:14:11 +0200

    Add: Tests for the Endpoint and Message

--- tests/src/coap_endpoint_tests.cpp
+++ tests/src/coap_endpoint_tests.cpp
@@ -1,17 +1,19 @@
 #include "catch.hpp"
 #include "full.h"
+#include <arpa/inet.h>
 
 SCENARIO("We should be able to use the CoAP layer","[coap]"){
     GIVEN("A CoAP Endpoint"){
         CoapEndpoint e("coap://127.0.0.1:9669/");
 
         WHEN("We send a message to the listening server"){
-            THEN("We check"){
+            THEN("It throws no exception"){
                 Message* mess = new Message();
                 mess->setDest("coap://127.0.0.1:9999/");
-                mess->setType(Message::NON_CONFIRMABLE);
-                mess->setHttpMethod(Message::GET);
+                mess->setType(MessageType::NON_CONFIRMABLE);
+                mess->setHttpMethod(HttpMethod::GET);
                 mess->setContent("Hola");
+                mess->addHeader(Headers::PURPOSE,"banana");
 
                 e.send(*mess);
             }
---
+++ tests/src/message_tests.cpp
@@ -0,0 +1,47 @@
+#include "../include/catch.hpp"
+#include "../../heapi/include/full.h"
+#include <arpa/inet.h>
+
+SCENARIO("We should be able to manipulate a message object","[message]"){
+    GIVEN("A message object"){
+        Message* mess = new Message();
+        mess->setDest("coap://127.0.0.1:9999/");
+        mess->setType(MessageType::NON_CONFIRMABLE);
+        mess->setHttpMethod(HttpMethod::GET);
+        mess->setContent("HolaBananezabonanana");
+        mess->addHeader(Headers::PURPOSE,"banana");
+
+        coap_session_t* sess;
+
+        std::string destination = "coap://127.0.0.1:9999/";
+        coap_uri_t* destParts = coap_new_uri(reinterpret_cast<const uint8_t*>(destination.c_str()),destination.length());
+        coap_address_t dstAddr;
+
+        coap_context_t* context;
+
+        context = coap_new_context(NULL);
+
+        coap_address_init(&dstAddr);
+        dstAddr.addr.sin.sin_family = AF_INET;
+
+        char uriBuf[destParts->host.length];
+        strncpy(uriBuf,reinterpret_cast<const char*>(destParts->host.s),destParts->host.length);
+
+        inet_pton(AF_INET, uriBuf, &dstAddr.addr.sin.sin_addr);
+        dstAddr.addr.sin.sin_port = htons(destParts->port);
+
+        sess = coap_new_client_session(context,NULL,&dstAddr,COAP_PROTO_UDP);
+
+        WHEN("Convert it to a pdu and back"){
+            coap_pdu_t* mess_coap = mess->toCoapMessage(sess);
+            Message mess2(sess,mess_coap);
+
+            THEN("Both messages have equal fields"){
+                REQUIRE(mess->getContent()==mess2.getContent());
+                //REQUIRE(mess->getDest()==mess2.getDest());
+                REQUIRE(mess->getType()==mess2.getType());
+                REQUIRE(mess->getHttpMethod()==mess2.getHttpMethod());
+            }
+        }
+    }
+}
