5795554367661738af41b001af800cb63d9962c1
SevenMoor <au.ottaviano@yahoo.com>
2021-04-20 23:54:25 +0200

    Update: Test for sending serialized object

--- tests/src/coap_endpoint_tests.cpp
+++ tests/src/coap_endpoint_tests.cpp
@@ -8,11 +8,14 @@ SCENARIO("We should be able to use the CoAP layer","[coap]"){
 
         WHEN("We send a message to the listening server"){
             THEN("It throws no exception"){
+                Matrix<int> a(3,3,MatrixArchetype::ID);
+                std::string body = Serializer::serialize(a);
+
                 Message* mess = new Message();
                 mess->setDest("coap://127.0.0.1:9999/");
                 mess->setType(MessageType::NON_CONFIRMABLE);
                 mess->setHttpMethod(HttpMethod::GET);
-                mess->setContent("Hola");
+                mess->setContent(body);
                 mess->addHeader(Headers::PURPOSE,"banana");
 
                 e.send(*mess);
